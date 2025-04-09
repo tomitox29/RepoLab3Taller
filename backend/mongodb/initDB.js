e960bb762c6d7cf5f7315c6799412a57fc4493c7
maxmigue <maxmigue2020@gmail.com>
2025-04-09 15:25:50 -0400

    refactor(mongoDB): Cambio en un console.log

--- backend/mongodb/initDB.js
+++ backend/mongodb/initDB.js
@@ -6,7 +6,7 @@ module.exports = {
     MongoDBConnection: async () => {
         try {
             await mongoose.connect(process.env.MONGODB_URI);
-            console.log("database connect")
+            console.log("[MONGO DB] Succesfully connected to Database");
         } catch (error) {
             console.log(error)
         }
